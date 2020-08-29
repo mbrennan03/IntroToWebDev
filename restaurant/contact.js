99b587efefe274819d5c25c440f27a28c1f9a768
mbrennan03 <63358124+mbrennan03@users.noreply.github.com>
2020-08-29 10:29:01 -0500

    updated data verification and deleted extra pics

--- restaurant/contact.js
+++ restaurant/contact.js
@@ -1,39 +1,25 @@
-function clearErrors() {
-    for (var loopCounter = 0; 
-        loopCounter < document.forms["submitRequest"].elements.length;
-        loopCounter++) {
-
-        if (document.forms["submitRequest"].elements[loopCounter]
-            .parentElement.className.indexOf("has-") != -1) {
-
-                document.forms["submitRequest"].elements[loopCounter]
-                    .parentElement.className = "form-group";
-            }
-    }
-}
-
 function validateItems() {
-    clearErrors();
     var name = document.forms["submitRequest"]["name"].value;
     var email = document.forms["submitRequest"]["email"].value;
     var phone = document.forms["submitRequest"]["phone"].value;
 
     if (name == "") {
         alert("Please enter your name.");
-        document.forms["submitRequest"]["name"].parentElement.parentElement.className = "form-group row has-error";
         document.forms["submitRequest"]["name"].focus();
         return false;
     }
     if (email == "") {
         alert("Please enter your email.");
-        document.forms["submitRequest"]["email"].parentElement.parentElement.className = "form-group row has-error";
         document.forms["submitRequest"]["email"].focus();
         return false;
     }
     if (phone.length != 10 || isNaN(phone)) {
         alert("Please enter your 10-digit phone number with no other characters.");
-        document.forms["submitRequest"]["phone"].parentElement.parentElement.className = "form-group row has-error";
         document.forms["submitRequest"]["phone"].focus();
+        return false;
+    }
+    if (!document.getElementById("choice1").checked && !document.getElementById("choice2").checked) {
+        alert("Please select if you have visited the restaurant before.")
         return false;
     }
 
