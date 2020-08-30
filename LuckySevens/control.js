f7148c2e473354f4e1cc90569cc2c096ef8a1fc5
mbrennan03 <63358124+mbrennan03@users.noreply.github.com>
2020-08-30 10:29:28 -0500

    updated game functions

--- LuckySevens/control.js
+++ LuckySevens/control.js
@@ -1,15 +1,46 @@
+var startingBet;
+var totalRolls = 0;
+var highestAmount = 0;
+var rollCount = 0;
+
 function verifyBet() {
-
-}
-
-function playGame() {
-
+    var amount = document.forms["sevens"]["bet"].value;
+    if (amount == "" || isNaN(amount) || amount <= 0) {
+        alert("Starting bet must be a positive number.");
+        document.forms["sevens"]["bet"].focus();
+        return false;
+    }
+    startingBet = amount;
+    highestAmount = amount;
+    playGame(amount);
+    document.getElementById("results").style.display = "block";
+    document.getElementById("submitButton").innerText = "Play Again";
+    document.getElementById("startingBet").innerText = startingBet;
+    document.getElementById("totalRolls").innerText = totalRolls;
+    document.getElementById("highestAmount").innerText = highestAmount;
+    document.getElementById("rollCount").innerText = rollCount;
+    return false;
 }
 
 function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
 
-function displayResults() {
+function playGame(amount) {
+    while(amount > 0) {
+        var roll1 = rollDice();
+        var roll2 = rollDice();
+        totalRolls++;
 
+        if (roll1 + roll2 == 7) {
+            amount += 4;
+        } else {
+            amount -= 1;
+        }
+
+        if (amount > highestAmount) {
+            highestAmount = amount;
+            rollCount = totalRolls;
+        }
+    }
 }
